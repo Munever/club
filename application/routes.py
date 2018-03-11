87c84f74e0f51099bf79eb5c5e47d3097de01cfa
Minever Abdurahmanova <Mineverabdurahmanova@yahoo.com>
2018-03-11 13:42:22 -0500

    update login method with User class

--- application/routes.py
+++ application/routes.py
@@ -1,32 +1,14 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from application.forms import LoginForm
 from application.models import User, Post
 from application import app
+from flask_login import logout_user, login_user
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'moto'}
-    posts = [
-        {
-            'title': {'main': 'I hate Python', 'sub': 'this is a sad story'},
-            'body': 'Python can do every thing. However, it is very hard.'
-        },
-        {
-            'title': {'main': 'I love Python', 'sub': 'this is a happy story'},
-            'body': 'Python can do every thing. However, it is very easy.'
-        },
-        {
-            'title': {'main': 'I hate Python', 'sub': 'this is a sad story'},
-            'body': 'Python can do every thing. However, it is very hard.'
-        },
-
-        {
-            'title': {'main': 'I love C#', 'sub': 'this is a happy story'},
-            'body': 'C# can do every thing. However, it is very easy.'
-        }
-    ]
-
+    user = {'username': 'Minever'}
+    posts = Post.query.all()
     return render_template('index.html', user=user, posts=posts)
 
 @app.route('/store')
@@ -53,7 +35,18 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+
+        return redirect('/Index')
 
     return render_template('login.html', title='Sign In', form=form)
+
+
 
 
